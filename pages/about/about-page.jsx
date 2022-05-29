4d7337ddef5c7dc59c5c7a2d10df95e82f452dc0
unknown <steen_hansen@yahoo.com>
2022-05-28 17:56:36 -0700

    final changes

--- pages/about/about-page.jsx
+++ pages/about/about-page.jsx
@@ -6,23 +6,7 @@ export { Page };
 function Page(_my_vars) {
   return (
     <div>
-      This program exists to help prepare multiple dishes for a meal using a phone in the kitchen. A list of my online{" "}
-      <a href="/steenhansen1942/gmail.com" className="base-link">
-        recipes
-      </a>
-      .
-      <br />
-      <br />
-      To call up a list of your open recipes with timers, click on 'Kitchen' as below.
-      <img className="mx-auto" src="about-user.png" alt="User View" width="278" height="624" />
-      <br />
-      This will display the two open recipes such as the below sample of my{" "}
-      <a href="/steenhansen1942/gmail.com/Pork%20Roast/steenhansen1942/gmail.com/Rice" className="base-link">
-        Pork Roast with rice
-      </a>{" "}
-      meal.
-      <img className="mx-auto" src="about-kitchen.png" alt="Kitchen View" width="278" height="586" />
-      <br />
+
       <div className="flex flex-wrap">
         <div className="flex-auto ml-1 text-xs w-[130px]">1 lb = 0.45 kg</div>
         <div className="flex-auto ml-1 text-xs w-[130px]">5 ml = 1 teaspoon</div>
@@ -33,22 +17,8 @@ function Page(_my_vars) {
         <div className="flex-auto ml-1 text-xs w-[130px]">1 qt = 4 cups = 0.95 L</div>
         <div className="flex-auto ml-1 text-xs w-[130px]">240 ml = 1 cup</div>
       </div>
-      <br />
-      <div id="filter-bottom-" className="flex justify-between clear-left ">
-        <div>
-          <a href="https://github.com/steenhansen/phone-recipes" className="base-link">
-            Program on GitHub
-          </a>
-        </div>
-        <span>
-          All icons are{" "}
-          <a href="https://www.flaticon.com/" className="base-link" title="Free icons">
-            Flaticons
-          </a>
-        </span>
-      </div>
-      <br />
-      <div className="flex flex-wrap">
+<br />
+  <div className="flex flex-wrap">
         <div className="flex-auto mb-2 text-xs font-thin">
           <img src="cow.png" alt="https://www.flaticon.com/free-icons/cow" title="Cow icons created by Freepik - Flaticon" width="68" height="68" />
           <div className="w-[68px]">Roast/Steak 145f/65c Ground 160f/70c</div>
@@ -89,6 +59,55 @@ function Page(_my_vars) {
           <div className="w-[68px]">Salmon 125f Halibut 130f Lobster 140f Scallops 130f Shrimp 120f</div>
         </div>
       </div>
+
+
+
+
+
+
+
+
+
+
+      This program exists to help prepare multiple dishes for a meal using a phone in the kitchen. A list of my online{" "}
+      <a href="/steenhansen1942/gmail.com" className="base-link">
+        recipes
+      </a>
+      .
+      <br />
+      <br />
+      To call up a list of your open recipes with timers, click on 'Kitchen' as below.
+      <img className="mx-auto" src="about-user.png" alt="User View" width="278" height="624" />
+      <br />
+      This will display the two open recipes such as the below sample of my{" "}
+      <a href="/steenhansen1942/gmail.com/Pork%20Roast/steenhansen1942/gmail.com/Rice" className="base-link">
+        Pork Roast with rice
+      </a>{" "}
+      meal.
+      <img className="mx-auto" src="about-kitchen.png" alt="Kitchen View" width="278" height="586" />
+      <br />
+
+
+
+      <div id="filter-bottom-" className="flex justify-between clear-left ">
+        <div>
+          <a href="https://github.com/steenhansen/phone-recipes" className="base-link">
+            Program on GitHub
+          </a>
+        </div>
+        <span>
+          All icons are{" "}
+          <a href="https://www.flaticon.com/" className="base-link" title="Free icons">
+            Flaticons
+          </a>
+        </span>
+      </div>
+      <br />
+
+    
+
+
+
       <div className="leading-3">
         <a href="https://www.flaticon.com/free-icons/bison" title="Bison icons" className="flaticon">
           Bison icon created by Freepik - Flaticon
