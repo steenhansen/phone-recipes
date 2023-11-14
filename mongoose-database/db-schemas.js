1ba81c1a6c7a0c0bd1ee8896fc4bb21e9d26f680
unknown <steen_hansen@yahoo.com>
2023-11-14 12:27:50 -0800

    rational

--- mongoose-database/db-schemas.js
+++ mongoose-database/db-schemas.js
@@ -1,7 +1,26 @@
+/*
+Note the complicated _ids and comments needed for linking the two tables.
+
+It was done this way to easily retreive all of a users comments quickly without table scanning.
+
+"My Scary Haggis" and "Green Eggs and Ham" recipes from email@gmail.com
+
+{ _id: "email@gmail.com~my scary haggis",
+ cook: "email@gmail.com",
+title: "My Scary Haggis" }
+
+{ _id: "email@gmail.com~green eggs and ham",
+ cook: "email@gmail.com",
+title: "Green Eggs and Ham",
+comments: ["email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~cut the sugar",
+           "email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~add some salt"] 
+}
+*/
+
 const RECIPES_SCHEMA = {
   _id: String, // email@gmail.com~green eggs and ham
   cook: String, // email@gmail.com
-  title: String, // green eggs and ham
+  title: String, // Green Eggs and Ham
   steps: String, // 'step1 step b'
   serves: String, // 'step1 step b'
   time: String, // 'step1 step b'
@@ -14,6 +33,18 @@ const RECIPES_SCHEMA = {
   comments: [String], // email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~cut the sugar
   search: String,
 };
+
+/*
+"Cut the Sugar" and "Add Some Salt" comments from bilbobaggins on the recipe "email@gmail.com~green eggs and ham"
+
+{ _id: "email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~cut the sugar",
+  by: "bilbobaggins@facebook.com",
+  title: "Cut the Sugar" }
+
+{ _id: "email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~add some salt",
+  by: "bilbobaggins@facebook.com",
+  title: "Add Some Salt" }
+*/
 
 const COMMENTS_SCHEMA = {
   _id: String, // email@gmail.com~green eggs and ham~bilbobaggins@facebook.com~cut the sugar
